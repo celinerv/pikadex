d4fa43062d0ab6c3ecd4b72589d5634f038322b2
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2018-12-25 21:14:14 -0500

    Reformat files, refactored token into private external resource

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,40 @@ import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import javax.security.auth.login.LoginException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main extends ListenerAdapter {
+
+    final static String secretsPath = "resources/secrets.json";
+
     public static void main(String[] args) throws LoginException {
+
+        // Read the auth token for JDA
+        JSONParser jsonParser = new JSONParser();
+        String token;
+        try {
+            JSONObject obj = (JSONObject)jsonParser.parse(new FileReader(secretsPath));
+            token = (String) obj.get("token");
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            return;
+        }
+
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "NTI3MjE0NjU4MTg1MDY4NTY0.DwQyoA.4_Igf5RMYdCaVF_dhHrzJFHqTac";
+
+        // Authenticate with JDA
         builder.setToken(token);
+
+        // Handle events
         builder.addEventListener(new Main());
+
+        // Launch bot
         builder.buildAsync();
     }
 
@@ -20,9 +45,8 @@ public class Main extends ListenerAdapter {
                 event.getAuthor().getName() + ":" +
                 event.getMessage().getContentDisplay()
         );
-        String response = "";
 
-
+        String response = null;
         switch(event.getMessage().getContentRaw()){
             case "!ping":
                 response = "Pong!";
@@ -35,17 +59,8 @@ public class Main extends ListenerAdapter {
                 break;
         }
 
-        event.getChannel().sendMessage(response).queue();
-
-        /*
-
-        if(event.getMessage().getContentRaw().equals("!ping")) {
-            event.getChannel().sendMessage("Pong!").queue();
-         }
-        if(event.getMessage().getContentRaw().equals("bitch")) {
-            event.getChannel().sendMessage("That's not nice").queue();
-        }
-        */
+        if (response == null) return; //do nothing
+        else event.getChannel().sendMessage(response).queue();
         }
     }
 
