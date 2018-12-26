d911b2ec467e9ee97cd8d94041c06cd25a8b56a6
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2018-12-25 22:13:35 -0500

    Hide secrets, Refactor EventListener

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,50 +2,40 @@ import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import javax.security.auth.login.LoginException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
+
+        // Read token from json
+        String token;
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader("rsrc/secrets.json"));
+            JSONObject json = (JSONObject) obj;
+            token = (String) json.get("token");
+        } catch (IOException | ParseException e){
+            e.printStackTrace();
+            return;
+        }
+
+        // Building the Bot...
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "NTI3MjE0NjU4MTg1MDY4NTY0.DwQyoA.4_Igf5RMYdCaVF_dhHrzJFHqTac";
+
+        // Authenticate
         builder.setToken(token);
-        builder.addEventListener(new Main());
+
+        // Attach Event Listener
+        builder.addEventListener(new EventListener());
+
+        // Build the bot
         builder.buildAsync();
     }
-
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event){
-        System.out.println("We received a message from" +
-                event.getAuthor().getName() + ":" +
-                event.getMessage().getContentDisplay()
-        );
-        String response = "";
-
-
-        switch(event.getMessage().getContentRaw()){
-            case "!ping":
-                response = "Pong!";
-                break;
-            case "bitch":
-                response = "That's not nice";
-                break;
-            default:
-                //do nothing
-                break;
-        }
-
-        event.getChannel().sendMessage(response).queue();
-
-        /*
-
-        if(event.getMessage().getContentRaw().equals("!ping")) {
-            event.getChannel().sendMessage("Pong!").queue();
-         }
-        if(event.getMessage().getContentRaw().equals("bitch")) {
-            event.getChannel().sendMessage("That's not nice").queue();
-        }
-        */
-        }
-    }
+}
 
